3dc5bf0940475c3a6fe27a869b36268b2ca57c68
aynkhn <167142752+aynkhn@users.noreply.github.com>
2025-08-15 23:21:59 -0500

    calculate MA's, make ticker variable

--- stockbot.py
+++ stockbot.py
@@ -5,13 +5,12 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from matplotlib import pyplot as plt
 import yfinance as yf
+ticker = 'MSFT'
+
+ticker = yf.Ticker(ticker) # initilize ticker - microsoft
+msft_history = ticker.history(period='5mo') # get stock history of 5 motnh
+df = pd.DataFrame(msft_history)
+df = df.drop(['Open', 'Low', 'Close', 'Volume', 'Dividends', 'Stock Splits'], axis=1).rename(columns={'High': 'Price'})
+df['MA5'] = df.rolling(window=5)['Price'].mean()
+df['MA20'] = df.rolling(window=20)['Price'].mean()
  
-today = pd.Timestamp.today(tz="America/New_York").normalize()
-past_20 = today - pd.Timedelta(days=20)
-past_5 = today - pd.Timedelta(days=5)
- 
-msft = yf.Ticker("MSFT")
-msft_history = msft.history(period="1mo")
-msft_high_20 = msft_history.loc[past_20:today, "High"].tolist()
- 
-print(msft_high_20)
