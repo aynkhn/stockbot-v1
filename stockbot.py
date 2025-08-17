32301809b8f36f1365ec36dd94c271ac037447ca
aynkhn <167142752+aynkhn@users.noreply.github.com>
2025-08-16 20:35:41 -0500

    working regression

--- stockbot.py
+++ stockbot.py
@@ -8,15 +8,29 @@ import yfinance as yf
 ticker = 'MSFT'
 
 ticker = yf.Ticker(ticker) # initilize ticker - microsoft
-msft_history = ticker.history(period='5mo') # get stock history of 5 motnh
+msft_history = ticker.history(period='1y') # get stock history of 5 motnh
 df = pd.DataFrame(msft_history)
-df = df.drop(['Open', 'Low', 'Close', 'Volume', 'Dividends', 'Stock Splits'], axis=1).rename(columns={'High': 'Price'})
-df['MA5'] = df.rolling(window=5)['Price'].mean()
-df['MA20'] = df.rolling(window=20)['Price'].mean()
-df['MADF'] = df['MA5'] - df['MA20']
+df = df[['High']] # drop everything but high values
+df['MA5'] = df.rolling(window=5)['High'].mean() # MA's
+df['MA20'] = df.rolling(window=20)['High'].mean()
+df['MADF'] = df['MA5'] - df['MA20'] # MA difference
+df['Target'] = df['High'].shift(-1)
+df = df.dropna() # drop NaN
+dfx = df[['MA5', 'MA20', 'MADF']] # X
+dfy = df['Target'] # Y
 
-dfx = df[['MA5', 'MA20', 'MADF']]
-dfy = df['Price'].shift(-1)
+x_train, x_test, y_train, y_test = train_test_split(dfx, dfy) # split train and test data
 
+LR = LinearRegression() # blank regression LR
+LR.fit(x_train, y_train) # fit LR to data
 
-print(df)
+y_pred = LR.predict(x_test) # run test data
+
+# display code
+plt.scatter(y_test, y_pred, color='blue', label='Test Data')
+plt.plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()], color='red', lw=2, label='Regression')
+plt.xlabel("Actual High")
+plt.ylabel("Predicted High")
+plt.title("Linear Regression Predictions")
+plt.legend()
+plt.show()
