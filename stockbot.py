2a86ee2d11eae8e466771c6f326ec8a84be52e7a
aynkhn <167142752+aynkhn@users.noreply.github.com>
2025-08-25 19:27:12 -0500

    changed train and test to different dates

--- stockbot.py
+++ stockbot.py
@@ -7,26 +7,31 @@ from matplotlib import pyplot as plt
 import yfinance as yf
 ticker = 'MSFT' # interchangable
 
+def df_actions(df):
+    df = df[['High']] # drop everything but high values
+    df['MA5'] = df['High'].rolling(window=5).mean() # MA's
+    df['MA20'] = df['High'].rolling(window=20).mean()
+    df['MADF'] = df['MA5'] - df['MA20'] # MA difference
+    df['Target'] = df['High'].shift(-1)
+    return df.dropna() # drop NaN
+
+## TRAINING DATA ##
 ticker = yf.Ticker(ticker) # initilize ticker
-msft_history = ticker.history(period='1y') # get stock history of 5 motnh
-df = pd.DataFrame(msft_history)
-df = df[['High']] # drop everything but high values
-df['MA5'] = df['High'].rolling(window=5).mean() # MA's
-df['MA20'] = df['High'].rolling(window=20).mean()
-df['MADF'] = df['MA5'] - df['MA20'] # MA difference
-df['Target'] = df['High'].shift(-1)
-df = df.dropna() # drop NaN
-dfx = df[['MA5', 'MA20', 'MADF']] # X
-dfy = df['Target'] # Y
+train_df = ticker.history(start="2020-01-01", end="2024-12-31") # get stock history
+test_df = ticker.history(start="2025-01-01", end="2025-12-31")
+train_df = df_actions(train_df)
+test_df = df_actions(test_df)
+x_train = train_df[['MA5', 'MA20', 'MADF']] # X train
+y_train = train_df['Target'] # Y train
+x_test = test_df[['MA5', 'MA20', 'MADF']] # X test
+y_test = test_df['Target'] # Y test
 
-x_train, x_test, y_train, y_test = train_test_split(dfx, dfy, test_size=0.25, random_state=42) # split train and test data, seeded
-
+## REGRESSION ## 
 LR = LinearRegression() # blank regression LR
 LR.fit(x_train, y_train) # fit LR to data
 
+## TEST AND DISPLAY ##
 y_pred = LR.predict(x_test) # run test data
-
-# display code
 plt.scatter(y_test, y_pred, color='blue', label='Test Data')
 plt.plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()], color='red', lw=2, label='Regression')
 plt.xlabel("Actual Price")
@@ -34,7 +39,6 @@ plt.ylabel("Predicted Price")
 plt.title("Linear Regression Predictions")
 plt.legend()
 plt.show()
-
 print("Coefficients:", LR.coef_) # testing
 print("Intercept:", LR.intercept_)
 print("R² score:", LR.score(x_test, y_test))
